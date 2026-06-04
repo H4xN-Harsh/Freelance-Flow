c7cf8c9788db64e8bdaf579e1d4505a39305a06b
H4xN-Harsh <harshxvillen74@gmail.com>
2026-06-04 12:22:07 +0530

    Backend setup

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const app = require('./src/app');
+const port = process.env.PORT||5000
+app.listen(port,()=>{
+    console.log(`server is running at : http://localhost:${port}`)
+})
