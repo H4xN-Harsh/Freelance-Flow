34dab25b74704c595408f26c21cb70a84eb15ebb
H4xN-Harsh <harshxvillen74@gmail.com>
2026-06-19 15:48:52 +0530

    drag and drop freature in progress!

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,18 +1,31 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
+import { useDraggable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities'; // ← ye install/import karo
 
 const TaskCard = ({ task }) => {
   const [isHovered, setHovered] = useState(false);
   const TaskClient = task.clientId?.clientName;
   const TaskClientEmail = task.clientId?.clientEmail;
   const taskTit = task.title;
-  const taskDets = task.description;
   const Taskstatus = task.status;
+
+  const { attributes, listeners, setNodeRef, transform ,isDragging } = useDraggable({ id: task._id });
+
+  
+  const style = transform ? {
+    transform: CSS.Translate.toString(transform),
+  } : undefined;
+
   return (
     <div
+      ref={setNodeRef}
+      style={style}  
+      {...attributes}
+      {...listeners}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className={`mt-3 glass-panel rounded-2xl p-6 transition-all duration-300 ease-in-out cursor-pointer ${
-        isHovered ? "scale-[1.03] shadow-lg shadow-brand/10" : "scale-100"
+      className={`mt-3 glass-panel rounded-2xl p-6 transition-all duration-300 ease-in-out cursor-grab ${
+        isDragging ? "scale-[1.03] shadow-lg shadow-brand/10" : "scale-100"
       }`}
     >
       <h3 className="text-lg font-semibold text-text-primary">{TaskClient}</h3>
